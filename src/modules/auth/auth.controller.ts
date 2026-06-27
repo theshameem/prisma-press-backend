54d4c3c3d56700c8b416cf4cae565392c88cf72a
theshameem75 <shameem.alam@selisegroup.com>
2026-06-27 18:21:38 +0600

    feat: token set in cookie

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,13 +7,27 @@ const loginUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
 
-    const loginResult = await authService.loginUser(payload);
+    const { accessToken, refreshToken } = await authService.loginUser(payload);
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
 
     sendResponse(res, {
       statusCode: 200,
       success: true,
       message: "User logged in successfully",
-      data: loginResult,
+      data: { accessToken, refreshToken },
     });
   },
 );
