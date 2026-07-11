24d74ad2cac5e55414390321d8d1778cb02359f8
theshameem75 <shameem.alam@selisegroup.com>
2026-07-11 20:32:55 +0600

    fix: stripe checkout api implementation

--- src/modules/subscription/subscription.controller.ts
+++ src/modules/subscription/subscription.controller.ts
@@ -1,8 +1,24 @@
 import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { subscriptionservice } from "./subscription.service";
 
 const createCheckoutSession = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.user?.id;
+
+    const result = await subscriptionservice.createCheckoutSession(
+      userId as string,
+    );
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Checkout completed successfully",
+      data: result,
+    });
+  },
 );
 
 export const subscriptionController = {
